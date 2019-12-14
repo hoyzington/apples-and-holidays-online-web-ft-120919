4468d3a3716cdb038ac1e5fd3e653da81a64745c
Scott Hoisington <hoyzington@gmail.com>
2019-12-14 00:21:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, hash_of_holidays|
     puts "#{season.capitalize}:"
     hash_of_holidays.each do |holiday, supplies_array|
-      puts "  #{holiday.capitalize}: #{supplies_array.join(", ")}"
+      puts "  #{holiday.to_s.capitalize}: #{supplies_array.join(", ")}"
     end
   end
 end
